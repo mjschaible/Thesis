ab04f0738d843e9606001903c9a8f746c4602aa9
mikey <ms5vf@virginia.edu>
2015-11-02 11:48:40 -0500

    Changes to be committed: 	modified:   TCcalcs.py 	modified:   multi_sint.py 	modified:   sintrates.py

--- sintrates.py
+++ sintrates.py
@@ -7,75 +7,11 @@
 # Tethys: Tlin = 1.9E7s, Tquad = 5.33E13s, Thertz = 5.33E13s
 # Dione: Tlin = 188s, Tquad = 1.49E12s , Thertz = 1.49E12s
 
-
 from decimal import Decimal
 from decimal import getcontext
 import numpy as np
 import matplotlib.pyplot as plt
 
-pi = np.pi
-kb = 1.38e-16 # erg/cm, Boltzman constant
-stoyear = 3.16888e-8 # multiply seconds by this to get years
-invkb = 1/kb
-
-Escale = 1e6 # Convert bar to erg/cm^3 (for mechanical constants)
-
-# Define several global (moon=Mimas) parameters
-kMout = 69 # erg/cm/s/K, Effective TC at Mimas outside the anomaly
-kMin = 1170 # erg/cm/s/K, Effective TC at Mimas inside the anomaly
-kTout = 6.53 # erg/cm/s/K, Effective TC at Tethys outside the anomaly
-kTin = 163 # erg/cm/s/K, Effective TC at Tethys inside the anomaly
-kDtrail = 17.1 # erg/cm/s/K, Effective TC at Dione on the trailing hemisphere
-kDlead = 32.4 # erg/cm/s/K, Effective TC at Dione on the leading hemisphere
-
-rg = 25E-4 # cm, approximate grain radius (25 um) (all bodies)
-phi = 0.5 # Assume 50% porosity for now (all bodies)
-Phi_elec_Mimas = 8.2e3 # elec/cm^2/s, incident electron flux at Mimas
-Phi_elec_Tethys = 2.1e2 # elec/cm^2/s, incident electron flux at Tethys
-Phi_elec_Dione = 2.8e2 # elec/cm^2/s, incident electron flux at Dione
-
-# For now assume an average energy loss for all depths
-# Should eventually use Penelope results to give vs. depth
-dEdz = (1e6)*1.602e-12 # (eV)erg/cm, energy deposition rate at Mimas
-
-# Perform calculations for which body?
-kin = kMin	
-kout = kMout
-Phi_elec = Phi_elec_Mimas
-
-# Define water ice parameters
-gamma_ss = 65 # erg/cm^2, ice/ice (grain boundary) surface energy
-gamma_sv = 109 # erg/cm^2, ice/vapor (free surface) surface energy
-delta_gb = 9.04e-8 # cm, grain boundary thickness
-delta_surf = 3.19e-8 # cm, effective surface (atomic layer) thickness (cube root of Omega)
-Omega = 3.25e-23 # cm^3, molar volume
-rho_ice = 0.934 # g/cm^3, density of water ice @ 80K
-WH2O = (27)*1.602e-12 # (eV)erg, average energy deposited in an excitation event
-
-radconst = dEdz*Phi_elec/WH2O
-
-# Calculate the approximate length scale of an electron excitation event 
-Ei = 5 # eV, heating energy to lattice
-U = 0.6 # eV/atom, characteristic energy of the solid
-Dz = 0.25*delta_surf*(2*Ei/U -1) # cm, length-scale of excitation event
-
-# ------ Define Wood model structural parameters -----
-Ysc = 0.09 # Obtained from curve fitting experimental data
-Zsc1 = 1 # Contact radius dependence (linear vs. quadratic), TBD....
-Zsc2 = 2 
-# Calculate Nc = Neighbor contacts, depends on the porosity from curve fitting  [Yang et al, 2000]
-Nc = 2.02*((1+87.38*(1-phi)**4)/(1+25.81*(1-phi)**4))
-Rconminlin = 0 # Minimum contact radius calculated with Wood theory
-Rconminquad = 0 # 'lin' and 'quad' assume linear and quadratic dependence 
-#Determine the Wood TC eqn pre-factor
-Apflin = (1/(Ysc*Nc))*(2*np.sqrt(Nc-1)*rg/Nc)**(Zsc1)
-Apfquad = (1/(Ysc*Nc))*(2*np.sqrt(Nc-1)*rg/Nc)**(Zsc2)
-
-# ------ Define Sirono and Yamamoto model parameters ------
-g = 4
-pc = 0.333
-p = 6*(1-phi)/pi
-SYpf = ((1-pc)/(p-pc))*g*rg*rg/pi
 
 def find_nearest(Rcon,Rcur):
     index = (np.abs(Rcon-Rcur)).argmin()
@@ -121,6 +57,85 @@ def fix_coeff(T):
 
     return tempSij
 
+# --------------- Define necessary model parameters -------------------------
+pi = np.pi
+kb = 1.38e-16 # erg/K, Boltzman constant
+stoyear = 3.16888e-8 # multiply seconds by this to get years
+invkb = 1/kb
+
+Escale = 1e6 # Convert bar to erg/cm^3 (for mechanical constants)
+
+# Define several global (moon=Mimas) parameters
+kMout = 69 # erg/cm/s/K, Effective TC at Mimas outside the anomaly
+kMin = 1170 # erg/cm/s/K, Effective TC at Mimas inside the anomaly
+kTout = 6.53 # erg/cm/s/K, Effective TC at Tethys outside the anomaly
+kTin = 163 # erg/cm/s/K, Effective TC at Tethys inside the anomaly
+kDtrail = 17.1 # erg/cm/s/K, Effective TC at Dione on the trailing hemisphere
+kDlead = 32.4 # erg/cm/s/K, Effective TC at Dione on the leading hemisphere
+
+rg = 25E-4 # cm, approximate grain radius (25 um) (all bodies)
+phi = 0.5 # Assume 50% porosity for now (all bodies)
+Phi_elec_Mimas = 8.2e3 # elec/cm^2/s, incident electron flux at Mimas
+Phi_elec_Tethys = 2.1e2 # elec/cm^2/s, incident electron flux at Tethys
+Phi_elec_Dione = 2.8e2 # elec/cm^2/s, incident electron flux at Dione
+
+# For now assume an average energy loss for all depths
+# Should eventually use Penelope results to give vs. depth
+dEdz = (1e6)*1.602e-12 # (eV)erg/cm, energy deposition rate at Mimas
+
+# Perform calculations for which body?
+kin = kMin
+kout = kMout
+Phi_elec = Phi_elec_Mimas
+
+# Define water ice parameters
+gamma_ss = 65 # erg/cm^2, ice/ice (grain boundary) surface energy
+gamma_sv = 109 # erg/cm^2, ice/vapor (free surface) surface energy
+delta_gb = 9.04e-8 # cm, grain boundary thickness
+delta_surf = 3.19e-8 # cm, effective surface (atomic layer) thickness (cube root of Omega)
+Omega = 3.25e-23 # cm^3, molar volume
+nH2O = 3e22 # molec/cm^3, molecular number density
+alat = 3e-8 # cm, approximate lattice spacing for H2O
+rho_ice = 0.934 # g/cm^3, density of water ice @ 80K
+WH2O = (27)*1.602e-12 # (eV)erg, average energy deposited in an excitation event
+UH2O_bulk = (0.7)*1.602e-12 # (eV) erg, bulk binding energy, 
+UH2O_surf = (0.1)*1.602e-12 # (eV) erg, surface binding energy, Watkins 2011
+
+R_iv = alat/2 # GUESS, separation distance for spontaneous vacancy/interstitial recombination
+alpha_v = 0.1 # GUESS, average number of point defects which escape cascade
+d_vac = 5e-6 # cm, approximate average sink diffusion length scale
+Nmob_surf = 0.8 * WH2O / (2 * UH2O_surf) # number of surface molecules mobilized per ionization
+Nmob_bulk = 0.8 * WH2O / (2 * UH2O_bulk)
+nj_surf = 15
+nj_bulk = 3
+
+invtau = dEdz*Phi_elec/(WH2O*nH2O)
+
+c_geo = 4 # geometric factor to determine sputtering/deposition relation
+
+# Calculate the approximate length scale of an electron excitation event 
+Ei = 5 # eV, heating energy to lattice
+U = 0.6 # eV/atom, characteristic energy of the solid
+Dz = 0.25*delta_surf*(2*Ei/U -1) # cm, length-scale of excitation event
+
+# ------ Define Wood model structural parameters -----
+Ysc = 0.22 # Obtained from curve fitting experimental data
+Zsc1 = 1 # Contact radius dependence (linear vs. quadratic), TBD....
+Zsc2 = 2 
+# Calculate Nc = Neighbor contacts, depends on the porosity from curve fitting  [Yang et al, 2000]
+Nc = 2.02*((1+87.38*(1-phi)**4)/(1+25.81*(1-phi)**4))
+Rconminlin = 0 # Minimum contact radius calculated with Wood theory
+Rconminquad = 0 # 'lin' and 'quad' assume linear and quadratic dependence 
+#Determine the Wood TC eqn pre-factor
+Apflin = (1/(Ysc*Nc))*(2*np.sqrt(Nc-1)*rg/Nc)**(Zsc1)
+Apfquad = (1/(Ysc*Nc))*(2*np.sqrt(Nc-1)*rg/Nc)**(Zsc2)
+
+# ------ Define Sirono and Yamamoto model parameters ------
+g = 4
+pc = 0.333
+p = 6*(1-phi)/pi
+SYpf = ((1-pc)/(p-pc))*g*rg*rg/pi
+
 # ----------- Begin main program -------------------------------------------
 
 T = np.linspace(60,110,num=51) # Define temperature range for calculations
@@ -128,22 +143,36 @@ invT = 1/T # Calculate the inverse of temperature
 Tnorm = T/273 # Calculate the normalized temprature (normalize to melting temp)
 
 # ----- Determine the thermal conductivity of ice -----
-kcem = [0.01, 0.1, 1.0, 2.0, 6.5] # [J/m/s/K], cementation TC
-kcem = map(lambda x: x*1e5, kcem)  # convert to [erg/cm/s/K]
+#kcem = [0.01, 0.1, 1.0, 2.0, 6.5] # [J/m/s/K], cementation TC
+#kcem = map(lambda x: x*1e5, kcem)  # convert to [erg/cm/s/K]
 kxice = (488.19/T+0.4685)*1e5 # [erg/cm/s/K], TC of Ih (hexagonal) crystalline water ice
 kaice = 7.1e2*T # [erg/cm/s/K], TC of amorphous water ice
 
 # ----- Calculate thermal diffusion coefficients for ice -----
-invR = 1/8.314 # [J/mol/K], gas constant
-#DsurfIc = 1.74e5 * np.exp(-38.2e3*invR*invT) # cm^2/s, from Kouchi 1994
+invR = 1/8.314 # [R] = [J/mol K], gas constant
+DsurfIc = 1.74e5 * np.exp(-38.2e3*invR*invT) # cm^2/s, from Kouchi 1994
 Dsurf = 3e2 * np.exp(-44.13e3*invR*invT) # cm^2/s, from Maeno and Ebinume, 1983
 DlatLDA = 7e-6*np.exp(-15e3*invR*invT) # from Ghesquiere 2015
 DlatIh = 1e-10*np.exp(-9e3*invR*invT) # from Ghesquiere 2015
 DlatB = 4.2e8*np.exp(-71.1e3*invR*invT) # from Livingston 1998
+Dvac = 1e-3*np.exp(-0.62*invT/8.62e-5) # from Hobbs, 1974
 Dgb = 8.4*np.exp(-49e3*invR*invT) # from ....
 
 # ----- Calculate the equilibrium vapor pressure of ice -----
 Pev = 3.65e13 * np.exp(-6141.7 * invT)
+
+# ----- Calculate radiation induced diffusion coefficients ----
+
+Deff_mc_surf = Nmob_surf*(nj_surf*alat)**2 *invtau/4
+Deff_mc_bulk = Nmob_bulk*(nj_bulk*alat)**2 *invtau/6
+
+Deff_esd_mid = 2*alpha_v*d_vac*d_vac*Nmob_bulk*invtau 
+Deff_esd_low = (alpha_v*Nmob_bulk*Dvac*invtau/(pi*nH2O*R_iv))**0.5
+
+Deff_surf = Deff_mc_surf + Deff_esd_mid + Deff_esd_low
+Deff_bulk = Deff_mc_bulk + Deff_esd_mid + Deff_esd_low
+
+phi_sput = c_geo*Dz*invtau*nH2O # sputtered flux
 
 # ----- Determine the compliance coefficients -----
 ltSij = np.zeros((6,6,len(T)))
@@ -170,8 +199,6 @@ Rconminquad = np.sqrt(Apfquad*kout*(1+0.5*phi)/(kxice*(1-phi)))
 Rconmaxquad = np.sqrt(Apfquad*kin*(1+0.5*phi)/(kxice*(1-phi)))
 RconminSY = np.sqrt((kout/kxice)*SYpf)
 RconmaxSY = np.sqrt((kin/kxice)*SYpf)
-chiminlin = (Rconminlin/(rg*0.4739))**4
-chiminquad = (Rconminquad/(rg*.4739))**4
 
 print "Rconminlin = %.2e" % (Rconminlin[20]*1E4), " um"
 print "Rconmaxlin = %.2e" % (Rconmaxlin[20]*1e4), " um"
@@ -180,30 +207,30 @@ print "Rconmaxquad = %.2e" % (Rconmaxquad[20]*1e4), " um"
 print "RconSYmax = %.2e" % (RconmaxSY[20]*1e4), " um"
 print "RconSYmin = %.2e" % (RconminSY[20]*1e4), " um"
 print "Rconjkr = %.2e" % (Rconjkr[20]*1e4), " um"
-'''print "Chiminlin = %.3e" % (chiminlin[1]/100), " um"
-print "Chiminquad = %.3e" % (chiminquad[1]/100), " um"
 
 fig = plt.figure(1)
-ax1 = fig.add_subplot(2,1,1)
+#ax1 = fig.add_subplot(2,1,1)
 #ax1.semilogy(T,Rconjkr*1E4, label = 'Rcon,jkr')
-ax1.semilogy(T,Rconminlin*1e4, label = 'Rcon,linear')
-ax1.semilogy(T,Rconminquad*1e4, label = 'Rcon,quad.')
-ax1.set_ylabel('Contact Radius [um]')
-ax1.axis([60,110,5e-3,1])
-ax1.legend(loc = 3, prop={'size':14})
+#ax1.semilogy(T,Rconminlin*1e4, label = 'Rcon,linear')
+#ax1.semilogy(T,Rconminquad*1e4, label = 'Rcon,quad.')
+#ax1.set_ylabel('Contact Radius [um]')
+#ax1.axis([60,110,5e-3,1])
+#ax1.legend(loc = 3, prop={'size':14})
 
-ax2= fig.add_subplot(2,1,2)
+ax2= fig.add_subplot(1,1,1)
+#ax2.semilogy(T,DsurfIc, label = 'DsurfIc')
 ax2.semilogy(T,Dsurf, label = 'Dsurf')
 ax2.semilogy(T,DlatB, label = 'Dlat')
 ax2.semilogy(T,Dgb, label = 'Dgb')
-#ax2.semilogy(T,Dradsurf, label = 'Dradsurf')
-#ax2.semilogy(T,Dradlat, label = 'Dradlat')
-#ax2.semilogy(T,Dradsput, label = 'Dradsput')
+ax2.semilogy(T,Dvac, label = 'Dvac')
+ax2.semilogy([60,110],[Deff_mc_bulk,Deff_mc_bulk], label = 'Drad_mc')
+ax2.semilogy([60,110],[Deff_esd_mid,Deff_esd_mid], label = 'Drad_esd, mid')
+ax2.semilogy(T,Deff_esd_low, label = 'Drad_esd, low')
 ax2.set_xlabel('Temperature [K]')
 ax2.set_ylabel('Diffusion Coeff. [cm^2/s]')
 ax2.legend(loc = 4, prop={'size':14})
 
-plt.savefig('./DiffusionRates.png',dpi=600) '''
+#plt.savefig('./DiffusionRates.png',dpi=600)
 
 # ----- Determine the variation in the sintering rates based on the contact radius -----
 Rcon_var = np.logspace(-6, -3, num=1000, base=10.0) # in cm
@@ -216,7 +243,7 @@ Km = invRcon_var - invRnc
 K3 = 2/rg
 
 # ----- Calculate radiation induced diffusion coefficients ----
-switches = [1,2,3]
+switches = [2]
 for dzswitch in switches:
     if dzswitch == 1:
         lineT = '-'
@@ -230,7 +257,6 @@ for dzswitch in switches:
         invTc=invT[0]
         Tc=T[0]
         Pevc=Pev[0]
-#        A = Dgb*delta_gb/(Dsurf*delta_surf)
         
     if dzswitch == 2:
         lineT = '--'
@@ -241,10 +267,13 @@ for dzswitch in switches:
         Dsurfc=Dsurf[20]
         Dgbc=Dgb[20]
         DlatBc=DlatB[20]
+        DlatIhc=DlatIh[20]
+        Dvacc=Dvac[20]
+        Deff_bulkc = Deff_bulk[20]
+        Deff_surfc = Deff_surf[20]
         invTc=invT[20]
         Tc=T[20]
         Pevc=Pev[20]
-  #       A = Dgb*delta_gb/(Dsurf*delta_surf)
         
     if dzswitch == 3:
         lineT = '-.'
@@ -255,6 +284,10 @@ for dzswitch in switches:
         Dsurfc=Dsurf[40]
         Dgbc=Dgb[40]
         DlatBc=DlatB[40]
+        DlatIhc=DlatIh[40]
+        Dvacc=Dvac[40]
+        Deff_bulkc = Deff_bulk[40]
+        Deff_surfc = Deff_surf[40]
         invTc=invT[40]
         Tc=T[40]
         Pevc=Pev[40]
@@ -289,22 +322,9 @@ for dzswitch in switches:
     dVdttherm_tot = dVdtsurf + dVdtlat + dVdtvap
     dRdttherm_tot = dVdttherm_tot/Vol_to_rad
 
-# ----- Calculate radiation induced diffusion coefficients ----
-    Veff_surf = 4*pi*delta_surf*Rcon_var*Dz
-    Veff_lat = 4*pi*Rcon_var*Dz*Dz
-    Veff_sput = Dz*rg*rg*(0.25-0.125*(np.sqrt(rg*rg-Rcon_var*Rcon_var)+rg)/rg)
-
-    inv_tau_surf = radconst*Veff_surf
-    inv_tau_lat = radconst*Veff_lat
-    inv_tau_sput = radconst*Veff_sput
-    
-    Dradsurf = Dz*Dz*inv_tau_surf
-    Dradlat = Dz*Dz*inv_tau_lat
-    Dradsput = Dz*Dz*inv_tau_sput
-
-    dVdtradsurf = 3*pi*Rcon_var*Dradsurf*delta_surf*gamma_sv*Omega*(K3-K2)*invkb*invTc/d2
-    dVdtradlat = 3*pi*Rcon_var*Dradlat*gamma_sv*Omega*(K3-Km)*invkb*invTc
-    dVdtradsput = 2*pi*Rcon_var*Dradsput*gamma_sv*Omega*invkb*invTc*(K3-Km)
+    dVdtradsurf = 3*pi*Rcon_var*Deff_surfc*delta_surf*gamma_sv*Omega*(K3-K2)*invkb*invTc/d2
+    dVdtradlat = 3*pi*Rcon_var*Deff_bulkc*gamma_sv*Omega*(K3-Km)*invkb*invTc
+    dVdtradsput = 2*pi*Rcon_var*theta*Rnc*(phi_sput/nH2O)*gamma_sv*Omega*invkb*invTc*(K3-Km)
 
     dRdtradsurf=dVdtradsurf/Vol_to_rad
     dRdtradlat=dVdtradlat/Vol_to_rad
@@ -313,13 +333,39 @@ for dzswitch in switches:
     dVdtrad_tot = dVdtradsurf + dVdtradlat + dVdtradsput 
     dRdtrad_tot = dVdtrad_tot/Vol_to_rad
 
+    fig = plt.figure(2)
+    ax2 = fig.add_subplot(1,1,1)
+    ax2.semilogy(Rcon_var/rg,dVdttherm_tot, linewidth=2, color='r', label = 'Therm. total')
+    ax2.semilogy(Rcon_var/rg,dVdtrad_tot, linewidth=2, color='b', label = 'Rad. total', linestyle = lineT)
+
+    ax2.semilogy([Rmin2/rg,Rmin2/rg],[1e-34,1e-24], color='k', linewidth=2, linestyle = '-.')
+    ax2.semilogy([Rmax2/rg,Rmax2/rg],[1e-34,1e-24], color='k', linewidth=2, linestyle = '-.')
+ 
+    if dzswitch == 2:
+        ax2.semilogy(Rcon_var/rg,dVdtradsurf, color='m', label = 'Rad. surface', linestyle = lineT)
+        ax2.semilogy(Rcon_var/rg,dVdtradlat, color = 'g', label = 'Rad. lattice', linestyle = lineT)
+        ax2.semilogy(Rcon_var/rg,dVdtradsput, color='c', label = 'Rad. sputter', linestyle = lineT)
+
+#        ax2.loglog(Rcon_var/rg,dVdtsurf, color='m', label = 'Therm. surface')
+#        ax2.loglog(Rcon_var/rg,dVdtlat, color = 'g', label = 'Therm. lattice')
+#        ax2.loglog(Rcon_var/rg,dVdtvap, color='c', label = 'Therm. vapor')
+
+        box = ax2.get_position()
+        ax2.set_xlabel('Contact/Grain radius (Rcon/rg)')
+        ax2.set_ylabel('Volumetric Sintering Rate [cm^3/s]')
+        ax2.set_xlim([0,0.1])
+        ax2.legend(loc = 3, prop={'size':12})
+        
+#    plt.savefig('./Rad_Sint_Rates.png',dpi=600)
+
+
     Rcur = Rmin2
  #   print 'The min rad is %.2e' % Rmin2, ' and the max is %.2e' % Rmax2
     ttot=0
     R_t = []
     timesteps = []
-    dt = 3.157e7
-#    print 'Rcon, dRdtrad_tot, dRdttherm_tot'
+    dt = 3.157e9
+    print 'Rcon, dRdtrad_tot, dRdttherm_tot'
 #    for i in range(10):
     while Rcur<Rmax2:
         index = find_nearest(Rcon_var,Rcur)
@@ -335,40 +381,6 @@ for dzswitch in switches:
 
     print 'the total time for %d K' % Tc, ' is %.2e' % ttot_years, ' years' 
 
-    fig = plt.figure(2)
-    ax2 = fig.add_subplot(2,1,2)
-    ax2.loglog(Rcon_var*1e4,dRdttherm_tot, linewidth=2, color='r', label = 'Therm. total', linestyle = lineT)
-    ax2.loglog(Rcon_var*1e4,dRdtrad_tot, linewidth=2, color='b', label = 'Rad. total', linestyle = lineT)
-
-    ax3 = fig.add_subplot(2,1,1)
-    ax3.semilogx([Rmin1*1e4,Rmin1*1e4],[0,1], color='k', label= 'T = %d' % Tc, linewidth=2, linestyle = lineT)
-    ax3.semilogx([Rmax1*1e4,Rmax1*1e4],[0,1], color='k', linewidth=2, linestyle = lineT)
-    ax3.semilogx([Rmin2*1e4,Rmin2*1e4],[0,1], color='b', linewidth=2, linestyle = lineT)
-    ax3.semilogx([Rmax2*1e4,Rmax2*1e4],[0,1], color='b', linewidth=2, linestyle = lineT)
-    ax3.legend(prop={'size':18},loc=4, bbox_to_anchor=(1,-3.5))
- 
-    if dzswitch == 1:
-        ax2.loglog(Rcon_var*1e4,dRdtradsurf, color='m', label = 'Rad. surface', linestyle = lineT)
-        ax2.loglog(Rcon_var*1e4,dRdtradlat, color = 'g', label = 'Rad. lattice', linestyle = lineT)
-        ax2.loglog(Rcon_var*1e4,dRdtradsput, color='c', label = 'Rad. sputter', linestyle = lineT)
-
-        box = ax2.get_position()
-        ax2.set_position([box.x0,box.y0,box.width,box.height*1.8])
-        ax2.set_xlabel('Contact Radius [um]')
-        ax2.set_ylabel('Radial Sintering Rate [cm/s]')
-        ax2.legend(loc = 3, prop={'size':18})
-        
-        box = ax3.get_position()
-        ax3.set_position([box.x0,box.y0+0.275,box.width,box.height*0.2])
-        plt.setp(ax3.get_yticklabels(), visible=False)
-        ax3.xaxis.tick_top()
-        ax3.xaxis.set_label_position('top')
-        ax3.set_xlabel('Contact Radius [um]')
-
-    
-    if dzswitch == 3:
-        plt.savefig('./Rad_Sint_Rates.png',dpi=600)
-
     fig = plt.figure(3)
     ax = fig.add_subplot(1,1,1)
     line, = ax.plot(timesteps,R_tarr*1e4, linewidth=2, color='r', label = 'Rad. sint @ %d' %Tc, linestyle = lineT)
@@ -378,7 +390,6 @@ for dzswitch in switches:
         ax.set_xlabel('Timestep [s]')
     if dzswitch == 3:
         ax.legend(loc = 3, prop={'size':18})
-        plt.savefig('./sint_timescales.png',dpi=600)
+#        plt.savefig('./sint_timescales.png',dpi=600)
 
 #plt.show()
-
